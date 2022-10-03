b8304aadfe76d78c76eec7db36fa170f780eb420
mkeleti <mmkeleti@gmail.com>
2022-10-03 17:20:51 -0500

    firsttry

--- pages/Camping.tsx
+++ pages/Camping.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useMantineTheme } from '@mantine/core'
-
+import  GroupTable from '../components/GroupTable';
 import Head from 'next/head'
 
 export default function Camping() {
     const theme = useMantineTheme()
+    const [groups, setGroups] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
     return (
         <>
             <Head>
                 <title>Camping</title>
             </Head>
-            camping
+            <GroupTable />
         </>
     )
 }
+
+
