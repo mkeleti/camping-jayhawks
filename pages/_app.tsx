41967f0ce658762f6c69105c33c7de16d2d6df30
mkeleti <mmkeleti@gmail.com>
2022-10-06 14:32:26 -0500

    add styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { MantineProvider } from '@mantine/core'
 import  Layout from '../components/layout';
+import '../styles/globals.css'
 
 export default function App(props: AppProps) {
     const { Component, pageProps } = props
