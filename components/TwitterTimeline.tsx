a24e902cd0f1eff3497b0b44315e8685db010bf7
mkeleti <mmkeleti@gmail.com>
2022-10-18 11:54:14 -0500

    Add-loading-animation

--- components/TwitterTimeline.tsx
+++ components/TwitterTimeline.tsx
@@ -1,16 +1,38 @@
-import { Paper } from '@mantine/core';
+import { Box, Paper, Skeleton } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
+const Skeletons = () => (<>
+  <Skeleton height={50} circle mb="xl" />
+  <Skeleton height={8} radius="xl" />
+  <Skeleton height={8} mt={6} radius="xl" />
+  <Skeleton height={8} mt={6} mb="md" width="70%" radius="xl" />
+                         </>
+);
+
 export default function Timeline() {
+  const [loading, setLoading] = useState(true);
+  function timing() {
+    setInterval(() => {
+      setLoading(false);
+    }, 5000);
+  }
+  useEffect(() => {
+    timing();
+  }, []);
   return (
         <Paper>
+                <Box sx={loading ? { display: 'none' } : { display: 'box' }}>
             <TwitterTimelineEmbed
               sourceType="profile"
               screenName="KUBBallCamping"
               noHeader
               noFooter
               transparent
+              onLoad={() => setLoading(false)}
             />
+                </Box>
+                <Box sx={loading ? { display: 'box' } : { display: 'none' }}><Skeletons /><Skeletons /><Skeletons /><Skeletons /><Skeletons /><Skeletons /><Skeletons /><Skeletons /></Box>
         </Paper>
   );
 }
