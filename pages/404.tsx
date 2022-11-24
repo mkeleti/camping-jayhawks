fe3e4d133485947b8751382732e509416cc751bc
Michael Keleti <mmkeleti@gmail.com>
2022-11-23 21:48:13 -0600

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next"
+import { Container, Paper, Title, Center, Card, Group, MediaQuery } from "@mantine/core"
+import Head from 'next/head';
+
+
+export const fourofour: NextPage = () => (
+  <>
+    <Head>
+      <title>404</title>
+    </Head>
+    <Container>
+      <Center>
+        <Title>404</Title>
+      </Center>
+      <Card radius="md" shadow="sm" p="md" mt="lg">
+        <Paper>
+          <Center>
+            <Title>Page Not Found</Title>
+          </Center>
+        </Paper>
+      </Card>
+    </Container>
+  </>
+);
+
+export default fourofour;
