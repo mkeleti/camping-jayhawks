68a0700081a0f1b90f843c8b884e8a65b91757d2
Michael Keleti <16996410+mkeleti@users.noreply.github.com>
2022-11-30 13:53:07 -0600

    done

--- pages/Camping.tsx
+++ pages/Camping.tsx
@@ -9,59 +9,74 @@ const Camping: NextPage = () => {
   const supabase = useSupabaseClient();
   const router = useRouter();
 
-
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => showPosition(position));
-    }
-    else {
+    } else {
       return false;
     }
-}
-
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let ku = {lat: 38.95834, long: -95.24751}
-  let correct_lat = (lat <= ku.lat + 0.01 && lat >= ku.lat - 0.01);
-  let correct_long = (long <= ku.long + 0.01 && long >= ku.long - 0.01);
-  if (correct_lat && correct_long) {
-    vote();
   }
-  else {
-    return false;
-  }
-}
 
-const user = useUser();
+  function showPosition(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let ku = { lat: 38.95834, long: -95.24751 };
+    let correct_lat = lat <= ku.lat + 0.01 && lat >= ku.lat - 0.01;
+    let correct_long = long <= ku.long + 0.01 && long >= ku.long - 0.01;
+    if (correct_lat && correct_long) {
+      vote();
+    } else {
+      return false;
+    }
+  }
+
+  const user = useUser();
   async function vote() {
     showNotification({
       autoClose: 2000,
       title: 'Roll Called!',
       message: 'Group Members have been notified of Roll Call.',
     });
-    const base = await supabase.from("voting").insert({suspend: false, email: [user.email],votes: 1});
+    const base = await supabase
+      .from('voting')
+      .insert({ suspend: false, email: [user.email], votes: 1 });
   }
   return (
-  <>
-    <Head>
-      <title>Camping</title>
-    </Head>
-    <Center>
-      <Title> Camping </Title>
-    </Center>
-    <Container>
-      <Card shadow="sm" mt="lg" radius="lg">
-        <Paper>
-          <SimpleGrid cols={2} mb="xl">
-            <Button
+    <>
+      <Head>
+        <title>Camping</title>
+      </Head>
+      <Center>
+        <Title> Camping </Title>
+      </Center>
+      <Container>
+        <Card shadow="sm" mt="lg" radius="lg">
+          <Paper>
+            <SimpleGrid cols={2} mb="xl">
+              {!user ? (
+                <Button
+                  color="green"
+                  onClick={() => {
+                    getLocation();
+                  }}
+                  disabled
+                >
+                  Roll Call
+                </Button>
+              ) : (
+                <Button
+                  color="green"
+                  onClick={() => {
+                    getLocation();
+                  }}
+                >
+                  Roll Call
+                </Button>
+              )}
+              {!user ? (
+              <Button
               color="green"
-              onClick={() => {getLocation()}}
-            >
-              Roll Call
-            </Button>
-            <Button
-              color="green"
+              disabled
               onClick={() => {
                 showNotification({
                   autoClose: 5000,
@@ -73,13 +88,29 @@ const user = useUser();
             >
               Suspend
             </Button>
-          </SimpleGrid>
-          <GroupTable />
-        </Paper>
-      </Card>
-    </Container>
-  </>
-);
-            }
+              ) : (
+                <Button
+                color="green"
+                onClick={() => {
+                  showNotification({
+                    autoClose: 5000,
+                    color: 'pink',
+                    title: 'Roll Call Suspended!',
+                    message: 'Roll Call has been suspended.',
+                  });
+                }}
+              >
+                Suspend
+              </Button>
+              )}
+
+            </SimpleGrid>
+            <GroupTable />
+          </Paper>
+        </Card>
+      </Container>
+    </>
+  );
+};
 
 export default Camping;
